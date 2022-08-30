3becee798d099b1f77ce6023857120339cefbcc1
CJ Henderson <chenderson1190@gmail.com>
2022-08-30 12:22:48 -0400

    add routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/list', function(req, res, next) {
+  // TODO: Make List Controller
+  res.render('list', {title: 'list'});
+});
+
+router.get('/favorites', function(req, res, next) {
+  // TODO: Make Favorites Page
+  res.render('error');
+});
+
+router.get('/updateInformation', function(req, res, next) {
+  //TODO: Make Info Update Form
+  res.render('error');
+});
 module.exports = router;
