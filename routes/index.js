0ca2107e614f10348945e7b4bcd521b12c93938d
CJ Henderson <chenderson1190@gmail.com>
2022-08-30 15:28:56 -0400

    update index, update app, begin prayer

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+<<<<<<< Updated upstream
+=======
+// Fire Store access Code
+const Firestore = require('@google-cloud/firestore');
+
+const db = new Firestore({
+  projectId: 'ctr-build-10',
+  keyFilename: '/ctr-build-10-945db5e1aa0d.json',
+});
+
+const ctrroster = db.collection('ctrroster');
+const names = await ctrroster.where('Name', '!=', null).get();
+if (names.empty) {
+  console.log('No Matches found');
+}
+names.forEach(doc => {
+  console.log(doc.id, '=>', doc.data());
+});
+
+>>>>>>> Stashed changes
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
