2f9a9ec3aeac701d8a42ce21b46655678b054dc0
CJ Henderson <chenderson1190@gmail.com>
2022-08-31 10:58:53 -0400

    make index.js more readable

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var rosterController = require('../controllers/rosterController')
 
-// Fire Store access Code
 const Firestore = require('@google-cloud/firestore');
 
+// Access Firestore Database
 const db = new Firestore({
   projectId: 'ctr-build-10',
   keyFilename: './ctr-build-10-945db5e1aa0d.json',
 });
 
-async function getData(){
+/**
+ * 
+ * @returns Roster Data in form of a list of Documents in Collection
+ */
+async function getRosterData(){
   const data = await db.collection('ctrroster').orderBy("Name").get();
   if (data.empty){
       return -1
     }
     else return data
 }
-const ctrlist = []
-getData().then(function(result){
-  console.log(result);
-  result.forEach(doc => {
-    console.log(doc.get('P Org'))
-    ctrlist.push(doc)
-  })
-})
 
+
+
+//console.log(ctrlist)
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+const ctrroster = []
+getRosterData().then(function(result){
+  result.forEach(doc => {
+    ctrroster.push(doc)
+  })
+})
+
 router.get('/list', function(req, res, next) {
-  // TODO: Make List Controller
-  console.log(ctrlist[1].get('Name'))
-  res.render('list', {roster: ctrlist});
-});
+  res.render('list', {roster: ctrroster})
+})
 
 router.get('/favorites', function(req, res, next) {
   // TODO: Make Favorites Page
