0c6de9b9d32f815f712f38915cf6da03a20f876d
CJ Henderson <chenderson1190@gmail.com>
2022-08-31 01:33:22 -0400

    Late Night Frind pt.2, got list working

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,24 @@ const Firestore = require('@google-cloud/firestore');
 
 const db = new Firestore({
   projectId: 'ctr-build-10',
-  keyFilename: '/ctr-build-10-945db5e1aa0d.json',
+  keyFilename: './ctr-build-10-945db5e1aa0d.json',
 });
+
+async function getData(){
+  const data = await db.collection('ctrroster').orderBy("Name").get();
+  if (data.empty){
+      return -1
+    }
+    else return data
+}
+const ctrlist = []
+getData().then(function(result){
+  console.log(result);
+  result.forEach(doc => {
+    console.log(doc.get('P Org'))
+    ctrlist.push(doc)
+  })
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,7 +32,8 @@ router.get('/', function(req, res, next) {
 
 router.get('/list', function(req, res, next) {
   // TODO: Make List Controller
-  res.render('list', {title: 'Hello World!'});
+  console.log(ctrlist[1].get('Name'))
+  res.render('list', {roster: ctrlist});
 });
 
 router.get('/favorites', function(req, res, next) {
