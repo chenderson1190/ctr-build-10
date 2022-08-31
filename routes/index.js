10a2b9067fa7a29b7467c523fe1446d72539b3bc
Isaac Attuah <isaacattuah@gmail.com>
2022-08-31 14:33:58 -0400

    Updated index.js with new pages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+
+const Firestore = require('@google-cloud/firestore');
+
+// Access Firestore Database
+const db = new Firestore({
+  projectId: 'ctr-build-10',
+  keyFilename: './ctr-build-10-945db5e1aa0d.json',
+});
+
+/**
+ * 
+ * @returns Roster Data in form of a list of Documents in Collection
+ */
+async function getRosterData(){
+  const data = await db.collection('ctrroster').orderBy("Name").get();
+  if (data.empty){
+      return -1
+    }
+    else return data
+}
+
+
+
+//console.log(ctrlist)
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+const ctrroster = []
+getRosterData().then(function(result){
+  result.forEach(doc => {
+    ctrroster.push(doc)
+  })
+})
+
 /* GET sign in page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sign In' });
@@ -16,11 +53,9 @@ router.get('/form', function(req, res, next) {
   res.render('form', { title: 'Form' });
 });
 
-
 router.get('/list', function(req, res, next) {
-  // TODO: Make List Controller
-  res.render('list', {title: 'Hello World!'});
-});
+  res.render('list', {roster: ctrroster})
+})
 
 router.get('/favorites', function(req, res, next) {
   // TODO: Make Favorites Page
@@ -32,3 +67,16 @@ router.get('/updateInformation', function(req, res, next) {
   res.render('error');
 });
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
