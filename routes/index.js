9fb1eab437b25d3cc3fe353a49136ac168d88afd
Isaac Attuah <isaacattuah@gmail.com>
2022-08-31 14:21:21 -0400

    Included Form

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,17 @@ var router = express.Router();
 
 /* GET sign in page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sign In' });
 });
 
 /* GET HOME page. */
 router.get('/home', function(req, res, next) {
   res.render('home', { title: 'Home' });
+});
+
+/* GET form page. */
+router.get('/form', function(req, res, next) {
+  res.render('form', { title: 'Form' });
 });
 
 
